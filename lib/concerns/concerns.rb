27bab8c1ca44b0512d93a3331c7d249f0c2a6f21
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-21 21:41:41 +0000

    Automatically backed up by Learn

--- lib/concerns/concerns.rb
+++ lib/concerns/concerns.rb
@@ -3,6 +3,6 @@ module Concerns::Findable
     self.all.find { |song| song.name == name }
   end
   def find_or_create_by_name(name)
-    find_by_name(name) || create(name)
+    self.find_by_name(name) || self.create(name)
   end
 end
