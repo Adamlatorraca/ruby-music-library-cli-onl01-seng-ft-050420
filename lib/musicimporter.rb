aa9d0f231b25a255facb44a6e817ccc1b6b17b8e
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-21 21:45:41 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -2,8 +2,8 @@ class MusicImporter
   attr_accessor :song, :genre, :artist
   @@all = []
 
-  def initialize(filename)
-    @filename = filename
+  def initialize(path)
+    @path = path
     @@all << self
   end
 
