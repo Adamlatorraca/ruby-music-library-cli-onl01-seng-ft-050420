d01137465ad6d679d02ec8506a1773c7341ca6f9
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-21 22:03:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
   def self.new_from_filename(filename)
     info = filename.split(" - ")
     artist, name, genre = info[0], info[1], info[2].gsub( ".mp3" , "")
-    
+
     genre = Genre.find_or_create_by_name(genre)
     artist = Artist.find_or_create_by_name(artist)
 
