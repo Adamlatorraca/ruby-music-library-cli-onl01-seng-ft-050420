59310456bf237c269d25e38004b8c3eaee0e598b
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-21 20:33:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,9 +29,9 @@ class Artist
   end
 
   def add_song(song)
-        song.artist = self unless song.artist
-        songs << song unless songs.include?(song)
-    end
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
 
 
   def songs
