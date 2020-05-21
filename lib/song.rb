f90c5bf2991ce7354e0a087411685c6d4b22b0bf
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-21 21:12:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,8 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) || self.create(name)
+    if self.find_by_name(name) == false
+       self.create(name)
     else
       name
     end
