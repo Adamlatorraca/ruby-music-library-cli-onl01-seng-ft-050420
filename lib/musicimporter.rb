3232d4bc5aa975973822683efebddc5b059ecfe8
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-21 22:11:04 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -11,7 +11,7 @@ class MusicImporter
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
-  def import(filename)
-    Song.create_from_filename(filename)
+  def import(files)
+    Song.create_from_filename(files)
   end
 end
