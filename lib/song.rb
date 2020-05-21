f567a497ce530e82e9ec63e2e43324b67b58a23a
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-21 21:07:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,10 +41,6 @@ class Song
     @@all.find { |song| song.name == name }
   end
 
-  def self.find(name)
-    self.all.find {|song| artist.name == name }
-  end
-
   def self.find_or_create_by_name(name)
    self.find(name) || self.create(name) : self.new(name)
   end
